ce09624824391209fa690ab78b40df713a9d28c8
Dominic <tientainguyen.coding@gmail.com>
2022-09-19 16:35:48 +0700

    feat: add service

--- tracker/src/app/components/task-item/task-item.component.ts
+++ tracker/src/app/components/task-item/task-item.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Task } from '../../Task';
 
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
-  styleUrls: ['./task-item.component.css']
+  styleUrls: ['./task-item.component.css'],
 })
 export class TaskItemComponent implements OnInit {
+  @Input() task!: Task;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
